9fbdff12fd9f442a3ecfeb25b6c1c144bbdc3b91
pepe <leandro0073@gmail.com>
2023-03-03 20:45:08 -0300

    Cambio de salida por pantalla

--- src/primer_ejercicio/Prueba.java
+++ src/primer_ejercicio/Prueba.java
@@ -3,7 +3,7 @@ package primer_ejercicio;
 public class Prueba {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo!!!");
+		System.out.println("Escribir una vez, ejecutar en cualquier parte!!!");
 		
 
 	}
--- src/primer_ejercicio/Prueba2.java
+++ src/primer_ejercicio/Prueba2.java
@@ -7,7 +7,7 @@ public class Prueba2 {
 	public static void main(String[] args) {
 		Scanner leer= new Scanner(System.in);
 		System.out.print("Ingrese un mensaje: ");
-		System.out.println("\n\t############\n\t" + leer.nextLine() + "\n\t###########");
+		System.out.println("\n\t@@@@@@@@@@@@@@\n\t" + leer.nextLine() + "\n\t@@@@@@@@@@@@");
 		
 		leer.close();
 	}
