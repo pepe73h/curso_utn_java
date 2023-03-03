c463fdc1ca6c0d649f9fd0dd23da8f875e2ffb70
pepe <leandro0073@gmail.com>
2023-03-02 23:56:09 -0300

    Se va la segunda

---
+++ src/primer_ejercicio/Prueba2.java
@@ -0,0 +1,17 @@
+package primer_ejercicio;
+
+import java.util.Scanner;
+
+public class Prueba2 {
+
+	public static void main(String[] args) {
+		Scanner leer= new Scanner(System.in);
+		System.out.print("Ingrese un mensaje: ");
+		System.out.println("\n\t############\n\t" + leer.nextLine() + "\n\t###########");
+		
+		leer.close();
+	}
+	
+}
+			
+
