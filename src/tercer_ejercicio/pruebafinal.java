deda1c34ac53dd59402b86f0a2eba52c1e476c55
pepe73h <126737505+pepe73h@users.noreply.github.com>
2023-03-06 19:08:50 -0300

    Update pruebafinal.java

--- src/tercer_ejercicio/pruebafinal.java
+++ src/tercer_ejercicio/pruebafinal.java
@@ -3,8 +3,7 @@ package tercer_ejercicio;
 public class pruebafinal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Esto no contiene nada");
 	}
 
 }
