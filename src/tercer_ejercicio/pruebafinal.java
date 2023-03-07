8e965fe480f46117f9ed94b0c1ffe59569c47b46
pepe <leandro0073@gmail.com>
2023-03-06 23:07:42 -0300

    Espero funcione

--- src/tercer_ejercicio/pruebafinal.java
+++ src/tercer_ejercicio/pruebafinal.java
@@ -3,7 +3,9 @@ package tercer_ejercicio;
 public class pruebafinal {
 
 	public static void main(String[] args) {
-		System.out.println("Vuelvo a modificar el archivo desde GitHub.");
+		System.out.println("Esto no contiene nada");
+		System.out.println("Modifico algo DISTINTO en mi repositorio local.");
+		
 	}
 
 
